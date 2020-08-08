82b0f8e3672e553f7c5d48b7eae1e43b08e4b5f0
Ruize Nie <rz376775@dal.ca>
2020-08-08 15:43:58 -0300

    configure the antd library for front end styling

---
+++ src/components/Button/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button as BaseButton } from "antd";
+import classnames from "classnames";
+
+import "./Button.scss";
+
+const Button = (props) => (
+  <BaseButton {...props} className={classnames("button", props.className)} />
+);
+
+Button.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Button;
